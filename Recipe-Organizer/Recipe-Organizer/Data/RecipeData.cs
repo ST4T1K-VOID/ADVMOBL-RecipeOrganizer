8a3bb4939ee40e606cbb8080c3bbab3dba3cf6f4
meyerj <20089460@tafe.wa.edu.au>
2026-02-06 14:14:22 +0800

    wip: adds initial design data for recipes.

--- Recipe-Organizer/Recipe-Organizer/Data/RecipeData.cs
+++ Recipe-Organizer/Recipe-Organizer/Data/RecipeData.cs
@@ -2,9 +2,25 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Recipe_Organizer.Data
+namespace Recipe_Organizer.Data;
+
+public static class RecipeList
 {
-    internal class RecipeList
-    {
-    }
+    public static Recipe[] Recipes => [
+        new(
+            name: "Porridge",
+            description: "Basic Porridge Recipe",
+            servings: 2,
+            preperationTime: 10.0
+            instructions: [
+                "Put rolled oats and water ihnto saucepan",
+                "Bring to boil on high",
+                "Reduce to simmer for 10 minutes"
+            ],
+            ingredients: [
+                ("Rolled oats", 1),
+                ("Water", 2)
+                ]
+            )
+        ];
 }
--- Recipe-Organizer/Recipe-Organizer/Recipe.cs
+++ Recipe-Organizer/Recipe-Organizer/Recipe.cs
@@ -4,7 +4,24 @@ using System.Text;
 
 namespace Recipe_Organizer
 {
-    internal class Recipe
+    public class Recipe
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Servings { get; set; }
+        public double PreperationTime { get; set; }
+        public List<string> Instructions { get; set; }
+        public List<(string, int)> Ingredients { get; set; }
+
+
+        public Recipe(string name, string description, int servings, double preperationTime, List<string> instructions, List<(string, int)> ingredients)
+        {
+            Name = name;
+            Description = description;
+            Servings = servings;
+            PreperationTime = preperationTime;
+            Instructions = instructions;
+            Ingredients = ingredients;
+        }
     }
 }
